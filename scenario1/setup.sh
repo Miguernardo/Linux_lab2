cd71060acae893dd1d2efd98e39ad02175ed329e
Bernarz Allen <87212022+Miguernardo@users.noreply.github.com>
2026-04-15 16:42:42 +0200

    Update setup.sh

--- scenario1/setup.sh
+++ scenario1/setup.sh
@@ -4,11 +4,11 @@ mkdir -p /home/ubuntu/lab
 cd /home/ubuntu/lab
 
 echo "Welcome to Linux Lab" > welcome.txt
-echo "This is a test file" > file1.txt
-echo "Another example file" > file2.txt
+echo "secret_key=alpha123" > hidden.txt
 
 mkdir data
 echo "log entry 1" > data/log.txt
-echo "log entry 2" >> data/log.txt
+
+chmod 777 hidden.txt
 
 chown -R ubuntu:ubuntu /home/ubuntu/lab
