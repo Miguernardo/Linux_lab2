0221b82b4c8032f2cdf70acdeda09c9741d41c1d
Bernarz Allen <87212022+Miguernardo@users.noreply.github.com>
2026-04-15 16:58:42 +0200

    Update verify.sh

--- scenario2/verify.sh
+++ scenario2/verify.sh
@@ -3,22 +3,15 @@
 FAIL=0
 
 grep -q "FLAG1_environment" /home/student/report.txt || FAIL=1
-grep -q "FLAG2_users" /home/student/report.txt || FAIL=1
 grep -q "FLAG3_hidden" /home/student/report.txt || FAIL=1
 
 PERM=$(stat -c "%a" /home/student/exploit.sh)
 if [ "$PERM" != "700" ]; then FAIL=1; fi
 
-PID=$(cat /tmp/malicious_pid)
-ps -p $PID > /dev/null 2>&1
-if [ $? -eq 0 ]; then FAIL=1; fi
-
-grep -q "FLAG6_puzzle" /home/student/report.txt || FAIL=1
-
 if [ $FAIL -eq 0 ]; then
-  echo "Mission Completed"
+  echo "Mission Complete"
   exit 0
 else
-  echo "Investigation incomplete"
+  echo "Still incomplete"
   exit 1
 fi
