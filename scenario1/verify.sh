b34d0ba1778f95d32b2c93f0235824ca6a2a8230
Bernarz Allen <87212022+Miguernardo@users.noreply.github.com>
2026-04-15 16:48:41 +0200

    Update verify.sh

--- scenario1/verify.sh
+++ scenario1/verify.sh
@@ -3,17 +3,15 @@
 FAIL=0
 
 grep -q "FLAG1_found_user" /home/ubuntu/lab/report.txt || FAIL=1
-grep -q "FLAG2_files_created" /home/ubuntu/lab/report.txt || FAIL=1
-grep -q "FLAG3_report_started" /home/ubuntu/lab/report.txt || FAIL=1
 grep -q "FLAG4_secret_found" /home/ubuntu/lab/report.txt || FAIL=1
 
 PERM=$(stat -c "%a" /home/ubuntu/lab/hidden.txt)
 if [ "$PERM" != "600" ]; then FAIL=1; fi
 
 if [ $FAIL -eq 0 ]; then
-  echo "Scenario 1 Completed"
+  echo "Completed"
   exit 0
 else
-  echo "Keep going"
+  echo "Incomplete"
   exit 1
 fi
