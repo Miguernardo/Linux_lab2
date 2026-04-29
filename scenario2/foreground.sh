533e07694990594b908eb703cf77ac601e611c30
Bernarz Allen <87212022+Miguernardo@users.noreply.github.com>
2026-04-29 03:49:39 +0200

    Update foreground.sh

--- scenario2/foreground.sh
+++ scenario2/foreground.sh
@@ -1,8 +1,11 @@
-# Création du chaos
+# Setup de la mission
 mkdir -p serveur/logs/old serveur/secret/archives serveur/.hidden_chamber
 echo "flag{INVISIBL3_MAST3R}" > serveur/.hidden_chamber/note.txt
-for i in {1..20}; do echo "Log line $i: Nothing to see here" > serveur/logs/file$i.log; done
+touch serveur/audit_initial.txt
+for i in {1..20}; do echo "Log line $i: No flags here" > serveur/logs/file$i.log; done
 echo "URGENT: flag{GR3P_PRO_99}" >> serveur/logs/file12.log
 echo "flag{LOCKSMITH_77}" > serveur/secret/archives/KEY
 chmod 000 serveur/secret/archives
-sleep 4000 & # Le processus à tuer
+sleep 4000 & 
+clear
+echo "MISSION CLEAN SWEEP : Le serveur est prêt. À vous de jouer."
